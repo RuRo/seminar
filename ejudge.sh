116cc21fe6ce8f01ade38bac5a997705556725de
ruro <ruro.ruro@ya.ru>
2018-03-03 16:34:08 +0300

    Modified ejudge.sh and index.md for 2nd semester tasks.

--- ejudge.sh
+++ ejudge.sh
@@ -14,9 +14,9 @@ else
     id=$(printf "%04d\n" $3)
     curl https://unicorn.ejudge.ru/ej/user/view-problem-submit/$1\?prob_id\=$3 -k --cookie "EJSID=$2" \
         | pcre2grep -Mo "<h3>(.|\n)*</h3>" \
-        | pandoc -s -r html -o ejudge1/$id.md
-    if [ "$(wc -m ejudge1/$id.md | grep -o '^[0-9]*')" -le 1 ]
+        | pandoc -s -r html -o ejudge2/$id.md
+    if [ "$(wc -m ejudge2/$id.md | grep -o '^[0-9]*')" -le 1 ]
     then
-        rm "ejudge1/$id.md"
+        rm "ejudge2/$id.md"
     fi
 fi
